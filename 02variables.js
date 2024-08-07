8ef8c7a3cf26cbd876b21c474e483cf41f9fbf48
Shivprasad3107 <100131634+Shivprasad3107@users.noreply.github.com>
2024-08-07 21:32:32 +0530

    Add files via upload

---
+++ 02variables.js
@@ -0,0 +1,16 @@
+const accountId=123435
+let accountEmail="Shiv@gmail.com"
+var accountpassword="12346"
+accountcity="Pune"
+let account;
+//accountId=2 not allowed as it is not allowed
+console.table([accountEmail,accountId,accountpassword,accountcity,account])
+// account chya corresponding te value undefined deta 
+// because aapan value assign nahi keliye
+accountEmail="hi@gmail.com"
+accountpassword="45678"
+accountcity="Sambhajinagar"
+console.log(accountId)
+console.table([accountEmail,accountId,accountpassword,accountcity])
+ // the thing which is lastly updated is seen in the the output window
+ // earlier it might have the other as aacountEmail accountpassword etc have different values earlier
---
+++ 03datatypes.js
@@ -0,0 +1,5 @@
+"use strict";//traets all the javascript code as the newer version of the code
+//alert( "hello")// we are using node js not the browser
+console.log(typeof"hiii")
+console.log( typeof null)
+console.log(typeof undefined)
