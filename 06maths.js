f5534b4f8774cd3655bd654a03cf049a58454d5b
Shivprasad3107 <100131634+Shivprasad3107@users.noreply.github.com>
2024-08-11 10:19:34 +0530

    Add files via upload

---
+++ 06maths.js
@@ -0,0 +1,12 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.min(2,4,5))
+
+console.log(Math.random()); // output hamesah 0 aur 1 ke bich ,e hi rahega
+const min=100
+const max=200 
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
