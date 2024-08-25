4ad9fc90a0989245e376ef868867703c552d0f16
Shivprasad3107 <100131634+Shivprasad3107@users.noreply.github.com>
2024-08-25 14:55:12 +0530

    Switch

---
+++ 16Switch.js
@@ -0,0 +1,39 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=1
+switch (month) {
+    case 1:
+        console.log("JAN")
+        break;
+  case 2:
+    console.log("FEB")
+    break;
+
+    case 3:
+        console.log("MARCH")
+        break;
+        case 4:
+            console.log("APRIL")
+            break;
+            case 5:
+                console.log("MAY")
+                break;
+                case 3:
+                    console.log("JUNE")
+                    break;
+                  
+
+    default:
+        console.log("Default case matched")
+        break;
+}
+
+// iF break  not written if any case mathches so all the code which is
+// written below that is executed except default code
